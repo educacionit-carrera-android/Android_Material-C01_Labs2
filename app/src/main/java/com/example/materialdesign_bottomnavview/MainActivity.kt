0ea9f1790c31b6db46aeb2272eaf78320d1053d8
carrera.android <carrera.android@educacionit.com>
2020-04-04 23:26:14 -0300

    Add labs2 resolution

--- app/src/main/java/com/example/materialdesign_bottomnavview/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign_bottomnavview/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.materialdesign_bottomnavview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +16,28 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.container, createHomeFragment())
             .commit()
+
+        setupBottomNavView()
+    }
+
+    private fun setupBottomNavView() {
+        bottomNavigationView = findViewById(R.id.bottomNavView)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            val transaction = supportFragmentManager
+                .beginTransaction()
+
+            val fragment = when (it.itemId) {
+                R.id.menuHome -> createHomeFragment()
+                R.id.menuFavoritos -> createFavoriteFragment()
+                R.id.menuMapa -> createMapFragment()
+                else -> createHomeFragment()
+            }
+
+            transaction
+                .replace(R.id.container, fragment)
+                .commit()
+            true
+        }
     }
 
     private fun createHomeFragment() = HomeFragment()
